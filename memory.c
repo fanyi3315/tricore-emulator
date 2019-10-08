0003952abe819f31f429531fee7ea2785f433739
Brandon Ros <brandonros1@gmail.com>
2019-10-07 23:33:52 -0400

    implement memory ranges

--- memory.c
+++ memory.c
@@ -9,13 +9,20 @@
 static uint8_t *pmu0 = NULL;
 static uint8_t *pmu1 = NULL;
 static uint8_t *external_flash = NULL;
+static uint8_t *c00000000 = NULL;
+static uint8_t *d00000000 = NULL;
 
 void set_memory_uint8_t(uint32_t address, uint8_t value) {
   printf("set_memory_uint8_t: %08x %02x\n", address, value);
-  bool valid_80000000_range = address >= 0x80000000 && address <= 0x8fffffff;
-  bool valid_d0000000_range = address >= 0xd0000000 && address <= 0xdfffffff;
-  bool valid_c0000000_range = address >= 0xc0000000 && address <= 0xcfffffff;
-  if (!valid_80000000_range && !valid_d0000000_range && !valid_c0000000_range) {
+  bool valid_d0000000_range = address >= 0xd0000000 && address <= 0xd000ffff;
+  bool valid_c0000000_range = address >= 0xc0000000 && address <= 0xc00fffff;
+  if (valid_d0000000_range) {
+    uint32_t offset = address - 0xd0000000;
+    d00000000[offset] = value;
+  } else if (valid_c0000000_range) {
+    uint32_t offset = address - 0xc0000000;
+    c00000000[offset] = value;
+  } else {
     fprintf(stderr, "set_memory_uint8_t: TODO: %08x\n", address);
     exit(1);
   }
@@ -23,10 +30,17 @@ void set_memory_uint8_t(uint32_t address, uint8_t value) {
 
 void set_memory_uint16_t(uint32_t address, uint16_t value) {
   printf("set_memory_uint16_t: %08x %04x\n", address, value);
-  bool valid_80000000_range = address >= 0x80000000 && address <= 0x8fffffff;
-  bool valid_d0000000_range = address >= 0xd0000000 && address <= 0xdfffffff;
-  bool valid_c0000000_range = address >= 0xc0000000 && address <= 0xcfffffff;
-  if (!valid_80000000_range && !valid_d0000000_range && !valid_c0000000_range) {
+  bool valid_d0000000_range = address >= 0xd0000000 && address <= 0xd000ffff;
+  bool valid_c0000000_range = address >= 0xc0000000 && address <= 0xc00fffff;
+  if (valid_d0000000_range) {
+    uint32_t offset = address - 0xd0000000;
+    d00000000[offset] = value & 0xFF;
+    d00000000[offset + 1] = (value >> 8) & 0xFF;
+  } else if (valid_c0000000_range) {
+    uint32_t offset = address - 0xc0000000;
+    c00000000[offset] = value & 0xFF;
+    c00000000[offset + 1] = (value >> 8) & 0xFF;
+  } else {
     fprintf(stderr, "set_memory_uint16_t: TODO: %08x\n", address);
     exit(1);
   }
@@ -34,10 +48,21 @@ void set_memory_uint16_t(uint32_t address, uint16_t value) {
 
 void set_memory_uint32_t(uint32_t address, uint32_t value) {
   printf("set_memory_uint32_t: %08x %08x\n", address, value);
-  bool valid_80000000_range = address >= 0x80000000 && address <= 0x8fffffff;
-  bool valid_d0000000_range = address >= 0xd0000000 && address <= 0xdfffffff;
-  bool valid_c0000000_range = address >= 0xc0000000 && address <= 0xcfffffff;
-  if (!valid_80000000_range && !valid_d0000000_range && !valid_c0000000_range) {
+  bool valid_d0000000_range = address >= 0xd0000000 && address <= 0xd000ffff;
+  bool valid_c0000000_range = address >= 0xc0000000 && address <= 0xc00fffff;
+  if (valid_d0000000_range) {
+    uint32_t offset = address - 0xd0000000;
+    d00000000[offset] = value & 0xFF;
+    d00000000[offset + 1] = (value >> 8) & 0xFF;
+    d00000000[offset + 2] = (value >> 16) & 0xFF;
+    d00000000[offset + 3] = (value >> 24) & 0xFF;
+  } else if (valid_c0000000_range) {
+    uint32_t offset = address - 0xc0000000;
+    c00000000[offset] = value & 0xFF;
+    c00000000[offset + 1] = (value >> 8) & 0xFF;
+    c00000000[offset + 2] = (value >> 16) & 0xFF;
+    c00000000[offset + 3] = (value >> 24) & 0xFF;
+  } else {
     fprintf(stderr, "set_memory_uint32_t: TODO: %08x\n", address);
     exit(1);
   }
@@ -102,4 +127,6 @@ void init_memory() {
   pmu1 = mmap(0, pmu1_s.st_size, PROT_READ, MAP_PRIVATE, pmu1_fd, 0);
   external_flash = mmap(0, external_flash_s.st_size, PROT_READ, MAP_PRIVATE,
                         external_flash_fd, 0);
+  c00000000 = malloc(0xFFFF);
+  d00000000 = malloc(0xFFFF);
 }
